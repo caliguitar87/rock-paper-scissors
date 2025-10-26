cf6646ca80957f46329038cbd543a14e5f8df1eb
Krystal <235606157+caliguitar87@users.noreply.github.com>
2025-10-26 12:00:52 -0700

    This is my new version with buttons and some javascript to make the game more responsive.

--- script.js
+++ script.js
@@ -16,75 +16,133 @@ function getComputerChoice() {
     return option;
 }
 
-function getHumanChoice() {
-    let choice = prompt("rock, paper or scissors:");
+let total = 0;
+let humanScore = 0;
+let computerScore = 0;
+let results = document.getElementById("results");
 
-    return choice.toLowerCase();
-}
-function playGame() {
-    humanScore = 0;
-    let total = 0;
-    computerScore = 0;
-    
 
-    function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {
 
-        if (humanChoice == computerChoice) {
-            console.log("It is a tie. No one wins this round.")
-        }
 
-        else {
-            if (computerChoice == "rock") {
-                if (humanChoice == "paper") {
-                    console.log("You win! Paper beats rock!");
-                    humanScore++;
+    if (humanChoice == computerChoice) {
+        results.textContent = "It is a tie. No one wins this round.";
+    }
 
-                }
-                else {
-                    console.log("You lose! Rock beats scissors.");
-                    computerScore++;
-                }
-            }
+    else {
+        if (computerChoice == "rock") {
+            if (humanChoice == "paper") {
+                results.textContent = "You win! Paper beats rock!";
+                humanScore++;
 
-            else if (computerChoice == "paper") {
-                if (humanChoice == "scissors") {
-                    console.log("You win! Scissor beats paper!");
-                    humanScore++;
-                }
-                else {
-                    console.log("You lose! Paper beats rock.");
-                    computerScore++;
-                }
             }
             else {
-                if (humanChoice == "rock") {
-                    console.log("You win! Rock beats scissors!");
-                    humanScore++;
-                }
-                else {
-                    console.log("You lose! Scissors beats paper.");
-                    computerScore++;
-                }
+                results.textContent = "You lose! Rock beats scissors.";
+                computerScore++;
             }
         }
 
+        else if (computerChoice == "paper") {
+            if (humanChoice == "scissors") {
+                results.textContent = "You win! Scissor beats paper!";
+                humanScore++;
+            }
+            else {
+                results.textContent = "You lose! Paper beats rock.";
+                computerScore++;
+            }
+        }
+        else {
+            if (humanChoice == "rock") {
+                results.textContent = "You win! Rock beats scissors!";
+                humanScore++;
+            }
+            else {
+                results.textContent = "You lose! Scissors beats paper.";
+                computerScore++;
+            }
+        }
     }
-    for ( let i =0; i < 5; i++ ) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        total++;
-    
-    if (total ==5){
-        console.log ("Final score after 5 rounds You:"+ humanScore +" vs. Computer:"+ computerScore+ ".");
-        if (humanScore > computerScore){
 
-        console.log("You win!");
-    }
-    else{
-        console.log("You lose!")
-    }
 }
 
-}}
-playGame();
+
+
+
+
+
+const gameOver = document.createElement("div");
+const body = document.querySelector("body");
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+
+
+rockBtn.addEventListener("click", () => {
+    const choice = rockBtn.innerHTML
+    if (total < 4) {
+        playRound(choice, getComputerChoice());
+
+        total++;
+    }
+    else {
+        if (humanScore>computerScore){
+            gameOver.textContent = "Final score after 5 rounds You:" + humanScore + " vs. Computer:" + computerScore + " You win! Game over.";
+        }
+        
+        else{
+            gameOver.textContent = "Final score after 5 rounds You:" + humanScore + " vs. Computer:" + computerScore + " You lose! Game over.";
+        }
+        }
+    
+
+})
+
+
+
+paperBtn.addEventListener("click", () => {
+    const choice = paperBtn.innerHTML
+
+    if (total <4) {
+        playRound(choice, getComputerChoice());
+
+        total++;
+    }
+    else {
+        if (humanScore>computerScore){
+            gameOver.textContent = "Final score after 5 rounds You:" + humanScore + " vs. Computer:" + computerScore + " You win! Game over.";
+        }
+        
+        else{
+            gameOver.textContent = "Final score after 5 rounds You:" + humanScore + " vs. Computer:" + computerScore + " You lose! Game over.";
+        }
+        }
+        body.appendChild(gameOver);
+
+       
+
+    }
+)
+scissorsBtn.addEventListener("click", () => {
+    const choice = scissorsBtn.innerHTML
+
+    if (total <4) {
+        playRound(choice, getComputerChoice());
+
+        total++;
+    }
+    else {
+        if (humanScore>computerScore){
+            gameOver.textContent = "Final score after 5 rounds You:" + humanScore + " vs. Computer:" + computerScore + " You win! Game over.";
+        }
+        
+        else{
+            gameOver.textContent = "Final score after 5 rounds You:" + humanScore + " vs. Computer:" + computerScore + " You lose! Game over.";
+        }
+        }
+        
+        body.appendChild(gameOver);}
+
+)
+
+
